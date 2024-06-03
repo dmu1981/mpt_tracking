fbe83432380c08d1b2139e8e35fcdfc91a0f3942
ModusMorris <maurice.191999@gmail.com>
2024-06-03 14:20:20 +0200

    only added examples from dennis

--- config.py
+++ config.py
@@ -1,5 +1,5 @@
 import dummy
-
+from nam import NamFilter
 # TODO: Add your filters here
 filters = {
     "Dummy": {
@@ -10,5 +10,9 @@ filters = {
         "constantturn": dummy.DummyFilter(2),
         "randomnoise": dummy.DummyFilter(2),
         "angular": dummy.DummyFilter(2),
+    },
+    "NAM_Team": {
+        "color": [0.5, 0.1, 0.9],
+        "constantposition": NamFilter(),
     }
 }
---
+++ nam.py
@@ -0,0 +1,12 @@
+import numpy as np
+
+
+class NamFilter:
+    def __init__(self):
+        pass
+
+    def reset(self, measurement):
+        return measurement[:2]
+
+    def update(self, dt, measurement):
+        return measurement[:2]
