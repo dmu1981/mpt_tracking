04500bf98bd5f0244d06c3bfd4ce37b0d930055a
Tim S. <timsan99@gmx.de>
2024-06-21 16:56:12 +0200

    Implemented Minimal-Variance Fusion for problem 1

--- config.py
+++ config.py
@@ -1,4 +1,5 @@
 import dummy
+import nofilter
 import tip_constantposition
 import tip_randomnoise
 
@@ -15,11 +16,20 @@ filters = {
     },
     "TIP": {
         "color": [0.169, 0.89, 0.725],
-        "constantposition": tip_constantposition.NoFilter(),
+        "constantposition": tip_constantposition.Minimal_Variance_Fusion(2),
         "constantvelocity": dummy.DummyFilter(2),
         "constantvelocity2": dummy.DummyFilter(2),
         "constantturn": dummy.DummyFilter(2),
         "randomnoise": tip_randomnoise.NoFilter(),
         "angular": dummy.DummyFilter(2),
+    },
+    "NO": {
+        "color": [1, 0.0, 0.0],
+        "constantposition": nofilter.NoFilter(),
+        "constantvelocity": nofilter.NoFilter(),
+        "constantvelocity2": nofilter.NoFilter(),
+        "constantturn": nofilter.NoFilter(),
+        "randomnoise": nofilter.NoFilter(),
+        "angular": nofilter.NoFilter(),
     }
 }
--- tip_constantposition.py
+++ tip_constantposition.py
@@ -1,11 +1,25 @@
 import numpy as np
 
-class NoFilter():
-  def __init__(self):
-    pass
+class Minimal_Variance_Fusion():
+    def __init__(self, shape):
+        self.shape = shape
+        self.measurements = []
+        self.cov_matrix = np.eye(shape) * 0.04  # initial cov-matrix as identity matrix
+        self.measurement_noise_matrix = np.eye(shape) * 0.04  # doesn't change (for updating cov-matrix later)
 
-  def reset(self, measurement):    
-    return measurement[:2]
-  
-  def update(self, dt, measurement):  
-    return measurement[:2]
+    def reset(self, measurement):
+        self.measurements = [np.array(measurement)]
+        self.cov_matrix = np.eye(self.shape) * 0.04
+        return self.measurements[0]
+
+    def update(self, dt, measurement):
+        z = np.array(measurement)
+        self.measurements.append(z)
+        
+        # New estimate x as the arithmetic mean of all measurements
+        self.x = np.mean(self.measurements, axis=0)
+
+        # Update covariance matrix
+        self.cov_matrix = self.measurement_noise_matrix / len(self.measurements)
+
+        return self.x
