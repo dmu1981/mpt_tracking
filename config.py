95e0102a4dc3dd870c9faf26be03c3d6e1f4658e
ModusMorris <maurice.191999@gmail.com>
2024-07-06 21:26:34 +0200

    added new imports and Filter problems

--- config.py
+++ config.py
@@ -1,13 +1,14 @@
 import numpy as np
 import dummy
-import constant_position
 import random_noise
+import angular
 import constantvelocity
 import constantvelocity2
 import nam
+import constant_turn2
+
 
 # TODO: Add your filters here
-
 filters = {
     "Dummy": {
         "color": [0.2, 0.2, 0.4],
@@ -18,21 +19,19 @@ filters = {
         "randomnoise": dummy.DummyFilter(2),
         "angular": dummy.DummyFilter(2),
     },
-  
     "NAMTeam": {
         "color": [0.5, 0.1, 0.9],
-
         "constantposition": nam.NamFilter(),
-        #"constantposition": constant_position.ConstantPositionFilter(),
-        "randomnoise": nam.RandomNoiseFilter(),
-        "angular": nam.AngularFilter() ,
-        "constantvelocity": dummy.DummyFilter(2),
-        "constantvelocity2": dummy.DummyFilter(2),
-        "constantturn": nam.ConstantTurnFilter(turn_rate=0.1),
+        "randomnoise": random_noise.RandomNoiseFilter(2),
+        #"randomnoise": nam.RandomNoiseFilter(),
+        #"angular": nam.AngularFilter() 
+        "constantvelocity": constantvelocity.KalmanFilterConstantVelocity(),
+        "constantvelocity2": constantvelocity2.KalmanFilterConstantVelocityMultiple(),
+        "constantturn": constant_turn2.ConstantTurnFilter(),
+        #"constantturn": nam.ConstantTurnFilter(turn_rate=0.1),
+        "angular": angular.AngularFilter(2),
 
     },
-    
-    
 }
 if __name__ == "__main__":
     print("This is the config file. Run main.py to start the simulation.")
