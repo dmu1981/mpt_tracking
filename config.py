df0226a7339d1684a6feefe9a6a0dbd6333410d3
Anas Alhariri <147917604+AlhaririAnas@users.noreply.github.com>
2024-07-10 19:25:20 +0200

    fine-tuning

--- config.py
+++ config.py
@@ -1,4 +1,3 @@
-import dummy
 import constanposition
 import constantvelocity
 import randomnoise
@@ -9,22 +8,15 @@ import constantvelocity2
 # TODO: Add your filters here
 
 filters = {
-    "Dummy": {
-        "color": [0.2, 0.2, 0.4],
-        "constantposition": dummy.DummyFilter(2),
-        "constantvelocity": constantvelocity.KalmanFilter(2),
-        "constantvelocity2": constantvelocity.KalmanFilter(2),
-        "constantturn": dummy.DummyFilter(2),
-        "randomnoise": dummy.DummyFilter(2),
-        "angular": dummy.DummyFilter(2),
-    },
     "Glotzkowski": {
         "color": [0.5, 0.3, 0.9],
         "constantposition": constanposition.KalmanFilter(2),
-        "randomnoise": randomnoise.KalmanFilter(2),
-        "constantturn": constantturn.ExtendedKalmanFilter(4, 20)
+        "constantvelocity": constantvelocity.KalmanFilter(2),
+        "constantvelocity2": constantvelocity2.ConstantVelocityKalmanFilter2(4, 2),
+        "constantturn": constantturn.Constantturn_KalmanFilter(4, 20),        
+        "randomnoise": randomnoise.Randomnoise_KalmanFilter(2),
+        "angular": angular_kalman.AngularKalmanFilter(2)
     }
-
 }
 
 # Aufruf mit python main.py --mode=constantposition --index=5
