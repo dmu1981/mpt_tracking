b9fd570e47ca26117504115b2fff322bf4b61837
Anas Alhariri <147917604+AlhaririAnas@users.noreply.github.com>
2024-07-10 20:39:03 +0200

    ready

--- config.py
+++ config.py
@@ -1,21 +1,14 @@
-import constanposition
-import constantvelocity
-import randomnoise
-import constantturn
-import angular_kalman
-import constantvelocity2
-
-# TODO: Add your filters here
+import glotzkowski
 
 filters = {
     "Glotzkowski": {
         "color": [0.5, 0.3, 0.9],
-        "constantposition": constanposition.KalmanFilter(2),
-        "constantvelocity": constantvelocity.KalmanFilter(2),
-        "constantvelocity2": constantvelocity2.ConstantVelocityKalmanFilter2(4, 2),
-        "constantturn": constantturn.Constantturn_KalmanFilter(4, 20),        
-        "randomnoise": randomnoise.Randomnoise_KalmanFilter(2),
-        "angular": angular_kalman.AngularKalmanFilter(2)
+        "constantposition": glotzkowski.Constantposition_KalmanFilter(2),
+        "constantvelocity": glotzkowski.Constantvelocity_KalmanFilter(2),
+        "constantvelocity2": glotzkowski.ConstantVelocityKalmanFilter2(4, 2),
+        "constantturn": glotzkowski.Constantturn_KalmanFilter(4, 20),        
+        "randomnoise": glotzkowski.Randomnoise_KalmanFilter(2),
+        "angular": glotzkowski.AngularKalmanFilter(2)
     }
 }
 
