72910a6092adc8fc86c4ca32f5fb66c7e4cab1d9
redibaj <149614500+redibaj@users.noreply.github.com>
2024-07-10 15:00:44 +0200

    Update filter_nick_redion.py
    Problem 1,3,4

--- filter_nick_redion.py
+++ filter_nick_redion.py
@@ -1,5 +1,35 @@
 import numpy as np
 
+# Problem 1
+class KalmanFilter():
+    def __init__(self, shape):
+        self.shape = shape
+        self.A = np.eye(shape)  # Zustandsübergangsmatrix
+        self.H = np.eye(shape)  # Beobachtungsmatrix
+        self.Q = np.eye(shape) * 0.0001  # Prozessrauschen
+        self.R = np.eye(shape) * 0.04  # Messrauschen
+        self.x = np.zeros(shape)  # Zustandsvektor
+        self.P = np.eye(shape)  # Kovarianzmatrix
+
+    def reset(self, measurement):
+        self.x = measurement[:self.shape]
+        self.P = np.eye(self.shape)
+        return self.x
+
+    def update(self, dt, measurement):
+        # Prediction
+        self.x = np.dot(self.A, self.x)
+        self.P = np.dot(self.A, np.dot(self.P, self.A.T)) + self.Q
+
+        # Update
+        S = np.dot(self.H, np.dot(self.P, self.H.T)) + self.R
+        K = np.dot(self.P, np.dot(self.H.T, np.linalg.inv(S)))
+        y = measurement[:self.shape] - np.dot(self.H, self.x)
+        self.x = self.x + np.dot(K, y)
+        self.P = self.P - np.dot(K, np.dot(self.H, self.P))
+
+        return self.x
+
 # Problem 2
 class RandomNoise:
     def __init__(self, shape):
@@ -30,7 +60,97 @@ class RandomNoise:
         self.P = (np.eye(self.shape) - K @ H) @ self.P  # Aktualisierung der Fehlerkovarianzmatrix
 
         return self.x
-    
+
+
+# Problem 3
+class ExtendedKalmanFilter():
+    def __init__(self, shape):
+        self.shape = shape
+        self.A = np.eye(shape)  # Zustandsübergangsmatrix
+        self.Q = np.eye(shape) * 0.0001  # Prozessrauschen
+        self.R = np.array([[0.0100, 0.0000], [0.0000, 0.0025]])  # Messrauschen
+        self.x = np.zeros(shape)  # Zustandsvektor
+        self.P = np.eye(shape)  # Kovarianzmatrix
+
+    def h(self, x):
+        """Konvertiert kartesische in polare Koordinaten"""
+        r = np.sqrt(x[0]**2 + x[1]**2)
+        phi = np.arctan2(x[1], x[0])
+        return np.array([r, phi])
+
+    def H_jacobian(self, x):
+        """Berechnet die Jacobi-Matrix der h-Funktion"""
+        r = np.sqrt(x[0]**2 + x[1]**2)
+        H = np.array([
+            [x[0] / r, x[1] / r],
+            [-x[1] / (r**2), x[0] / (r**2)]
+        ])
+        return H
+
+    def reset(self, measurement):
+        r, phi = measurement
+        self.x = np.array([r * np.cos(phi), r * np.sin(phi)])
+        self.P = np.eye(self.shape)
+        return self.x
+
+    def update(self, dt, measurement):
+        # Prediction
+        self.x = np.dot(self.A, self.x)
+        self.P = np.dot(self.A, np.dot(self.P, self.A.T)) + self.Q
+
+        # Update
+        H = self.H_jacobian(self.x)
+        z_pred = self.h(self.x)
+        y = measurement - z_pred
+
+        # Normalisiere Winkel
+        y[1] = (y[1] + np.pi) % (2 * np.pi) - np.pi
+
+        S = np.dot(H, np.dot(self.P, H.T)) + self.R
+        K = np.dot(self.P, np.dot(H.T, np.linalg.inv(S)))
+        self.x = self.x + np.dot(K, y)
+        self.P = self.P - np.dot(K, np.dot(H, self.P))
+
+        return self.x
+
+# Problem 4
+class KalmanFilterConstantVelocity():
+    def __init__(self, shape):
+        self.shape = shape
+        self.dim = shape // 2
+        self.A = np.eye(shape)
+        self.A[:self.dim, self.dim:] = np.eye(self.dim)
+        self.Q = np.eye(shape) * 0.0001  # Prozessrauschen
+        self.R = np.eye(self.dim) * 0.04  # Messrauschen
+        self.x = np.zeros(shape)  # Zustandsvektor (Position und Geschwindigkeit)
+        self.P = np.eye(shape)  # Kovarianzmatrix
+
+    def reset(self, measurement):
+        self.x[:self.dim] = measurement[:self.dim]
+        self.x[self.dim:] = 0
+        self.P = np.eye(self.shape)
+        return self.x[:self.dim]
+
+    def update(self, dt, measurement):
+        # Update the state transition matrix A with the new dt
+        self.A[:self.dim, self.dim:] = np.eye(self.dim) * dt
+
+        # Prediction
+        self.x = np.dot(self.A, self.x)
+        self.P = np.dot(self.A, np.dot(self.P, self.A.T)) + self.Q
+
+        # Update
+        H = np.eye(self.dim, self.shape)
+        z_pred = np.dot(H, self.x)
+        y = measurement[:self.dim] - z_pred
+
+        S = np.dot(H, np.dot(self.P, H.T)) + self.R
+        K = np.dot(self.P, np.dot(H.T, np.linalg.inv(S)))
+        self.x = self.x + np.dot(K, y)
+        self.P = self.P - np.dot(K, np.dot(H, self.P))
+
+        return self.x[:self.dim]
+
 
 # Problem 5
 class ConstantVelocity:
