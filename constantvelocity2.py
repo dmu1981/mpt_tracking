2f39aa910e8a0be42c66f159709be4e77ca8d200
heyitsalina <alina.weidemann04@gmail.com>
2024-07-10 13:43:37 +0200

    changing my filter

--- constantvelocity2.py
+++ constantvelocity2.py
@@ -1,65 +1,46 @@
 import numpy as np
 
 class ConstantVelocityKalmanFilter2:
-    # Ein Objekt befindet sich an einem unbekannten Ort und bewegt sich mit einer unbekannten (aber konstanten) Geschwindigkeit fort.
+    # Ein Objekt befindet sich an einem unbekannten Ort und bewegt sich mit einer unbekannten (aber konstanten) Geschwindigkeit fort
     # Es gibt 5 unabhängige Messungen mit jeweils unkorreliertem Messrauschen
     # Die Standardabweichung für jede Messung ist in jeder Phase zufällig
     # Die letzte 10 Dimensionen geben die jeweilige Standardabweichung des Meßrauschens an
     # ZIEL: Schätzung der 2-dimensionalen Position des Objekts
 
-    def __init__(self, state_size, measurement_size):
-        self.state_size = state_size  # Zustand: 4-dimensionaler Vektor aus Position und Geschwindigkeit (x, y, vx, vy)
-        self.measurement_size = measurement_size  # Messungen: 2-dimensionaler Vektor (x, y)
-
-        # Initialisierung des Zustands und der Kovarianzmatrix
-        self.x = np.zeros(self.state_size)
-        self.P = np.eye(self.state_size) * 1000 # Eine hohe Kovarianz = hohe Unsicherheit aufgrund mangelnder Infos
-
-        # Prozessrauschkovarianz Q
-        self.Q = np.eye(self.state_size) * 0.01 # Das Prozessrauschen wird modelliert
+    def __init__(self):
+        self.x = np.zeros(4)  # Zustandsvektor: 4 dimensional bestehend aus Position und Geschwindigkeit [x, y, vx, vy]
+        self.P = np.eye(4)    # Fehlerkovarianzmatrix: beschreibt Unsicherheit über x
 
     def reset(self, measurement):
-        # Initialisierung des Zustands mit dem Mittelwert der ersten fünf unabhängigen Messungen
-        z = measurement[:10]
-        self.x[:2] = np.mean(z, axis=0)  # Durchschnitt der Positionen
-        self.x[2:] = 0  # Anfangsgeschwindigkeit auf 0 setzen
-        self.P = np.eye(self.state_size) * 1000  # Kovarianz zurücksetzen
+        # gibt die schätzung für die position von x zurück
+        z = measurement[:10].reshape(5, 2)  # reshape teilt den Vektor in eine 5x2 Matrix auf = jede Zeile ist eine Positionsmessung
+        self.x = np.mean(z, axis=0)         # mittlerer Punkt aller Messungen
+        self.x = np.append(self.x, [0, 0])  # geschwindigkeitsvektor wird zu x hinzugefügt
+        self.P = np.eye(4)                  # hohe kovarianz = hohe unsicherheit 
         return self.x[:2]
 
     def update(self, dt, measurement):
-        # Extrahiere Messungen und Messrauschen
-        z = measurement[:10]
-        R_values = measurement[10:]
+        z = measurement[:10].reshape(5, 2)
+        R_diag = measurement[10:]
+        R = np.diag(R_diag)
+        
+        # F = Zustandsübergangsmatrix
+        F = np.array([[1, 0, dt, 0],
+                      [0, 1, 0, dt],
+                      [0, 0, 1, 0],
+                      [0, 0, 0, 1]])
+        self.x = F @ self.x                # Berechnung des "neuen" Zustands
+        self.P = F @ self.P @ F.T          # Berechnung der neuen Fehlerkovarianz 
 
-        # Zustandsübergangsmatrix F
-        F = np.array([
-            [1, 0, dt, 0],
-            [0, 1, 0, dt],
-            [0, 0, 1, 0],
-            [0, 0, 0, 1]
-        ])
-
-        # Vorhersage
-        self.x = F @ self.x
-        self.P = F @ self.P @ F.T + self.Q
-
-        # Kalman-Gewinnberechnung und Zustandsupdate für alle 5 Messungen (for schleife)
-        H = np.array([
-            [1, 0, 0, 0],
-            [0, 1, 0, 0]
-        ])
-
-        for i in range(5): 
-            z_i = z[i]
-            R_i = np.diag(R_values[i*2:i*2+2])
-
-            # Korrektur
-            y = z_i - H @ self.x
-            S = H @ self.P @ H.T + R_i
-            K = self.P @ H.T @ np.linalg.inv(S)
-
-            # Aktualisierung
-            self.x = self.x + K @ y
-            self.P = self.P - K @ H @ self.P
+    
+        z_mean = np.mean(z, axis=0)            # z_mean = Messvektor ist der Durschnitt der Positionsmessungen
+        H = np.array([[1, 0, 0, 0],            # H = Messmatrix
+                      [0, 1, 0, 0]])
+        S = H @ self.P @ H.T + R               # S = Innovationskovarianzmatrix
+        K = self.P @ H.T @ np.linalg.inv(S)    # K = Der Kalman Gain
+        
+        y = z_mean - H @ self.x[:2]              # y = Residualvektor
+        self.x = self.x + K @ y                  # finaler Zustand (Kalman Gain wird mit Residualvektor multipliziert)
+        self.P = (np.eye(4) - K @ H) @ self.P    # finale Fehlerkovarianz
 
         return self.x[:2]
