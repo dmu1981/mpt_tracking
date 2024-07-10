ee4b3b0cf6077e28287309f36b22abfcbc9b773e
Jxshyz <Joschua.Schramm@yahoo.de>
2024-07-10 17:31:01 +0200

    implemented the 4th problem - constantvelocity

--- config.py
+++ config.py
@@ -1,38 +1,19 @@
 import dummy
 import constanposition
+import constantvelocity
 import randomnoise
 
 # TODO: Add your filters here
 
 filters = {
-    "Dummy":{
+    "GLoTzKoWsKi":{
         "color": [0.2, 0.2, 0.4],
         "constantposition": dummy.DummyFilter(2),
-        "constantvelocity": dummy.DummyFilter(2),
-        "constantvelocity2": dummy.DummyFilter(2),
+        "constantvelocity": constantvelocity.KalmanFilter(2),
+        "constantvelocity2": constantvelocity.KalmanFilter(2),
         "constantturn": dummy.DummyFilter(2),
         "randomnoise": dummy.DummyFilter(2),
         "angular": dummy.DummyFilter(2),
-    },
-    "NoFilter": {
-        "color": [0.3, 0.3, 0.4],
-        "constantposition": constanposition.NoFilter(),
-        "randomnoise": dummy.DummyFilter(2),
-    },
-    "KalmanFilter": {
-        "color": [0.3, 0.3, 0.4],
-        "constantposition": constanposition.KalmanFilter(2),
-        "randomnoise": dummy.DummyFilter(2),
-    },
-    "SimpleNoiseFilter":{
-        "color": [0.1, 0.2, 0.3],
-        "constantposition": constanposition.SimpleNoiseFilter(2),
-        "randomnoise": dummy.DummyFilter(2),
-    },
-    "RandomNoiseFilter": {
-        "color": [0.1, 0.2, 0.3],
-        "constantposition": dummy.DummyFilter(2),
-        "randomnoise": randomnoise.RandomNoiseFilter(2),
     }
 }
 # Aufruf mit python main.py --mode=constantposition --index=5
