d28150eb1a34e4e744b99807d77b7a99ab2a5931
Tim S. <timsan99@gmx.de>
2024-07-11 00:14:33 +0200

    Corrected, linted, config.py altered

--- tip_constantvelocity2.py
+++ tip_constantvelocity2.py
@@ -1,15 +1,16 @@
 import numpy as np
 
+
 class constantvelocity2_EKF:
-    def _init_(self, shape):
+    def __init__(self, shape):
         self.dim_z = shape
-        self.dim_x = np.zeros(4)
+        self.dim_x = 4
         self.num_coordinates = 10
-        
+
         Matrix_I = np.eye(shape)
-        Matrix_I = np.vstack((Matrix_I,Matrix_I,Matrix_I,Matrix_I,Matrix_I))
-        self.H = np.hstack((Matrix_I,np.zeros((self.num_coordinates, self.dim_z))))
-        
+        Matrix_I = np.vstack((Matrix_I, Matrix_I, Matrix_I, Matrix_I, Matrix_I))
+        self.H = np.hstack((Matrix_I, np.zeros((self.num_coordinates, self.dim_z))))
+
         self.Q = np.zeros((self.dim_x, self.dim_x))
 
     def reset(self, measurement):
@@ -20,10 +21,7 @@ class constantvelocity2_EKF:
     def update(self, dt, measurement):
         z = np.array(measurement[: self.num_coordinates]).reshape(self.num_coordinates)
         R = np.diag(measurement[self.num_coordinates :])
-        F = np.array([[1, 0, dt, 0], 
-                           [0, 1, 0, dt], 
-                           [0, 0, 1, 0], 
-                           [0, 0, 0, 1]])
+        F = np.array([[1, 0, dt, 0], [0, 1, 0, dt], [0, 0, 1, 0], [0, 0, 0, 1]])
 
         # Process noise covariance matrix, scaled by dt
         Q = np.eye(self.dim_x) * 0.01 * dt
@@ -40,7 +38,7 @@ class constantvelocity2_EKF:
 
         # Kalman gain
         K = self.P @ self.H.T @ np.linalg.inv(S)
-        
+
         # To posterior state
         # Update prediction
         self.x = self.x + K @ y
